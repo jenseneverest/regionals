6cdf943b2033beb3a450ca64fe4669c40ff2a8d1
jenseneverest <jenseneverest@yahoo.com>
2019-11-21 19:34:24 +0000

    Add files via upload

---
+++ choice.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#Jenseneverest Crossepg setup for Virgin EPG data.
+echo ""
+echo "        **********************************************"
+echo "        *                                            *"
+echo "        *     WELCOME TO THE CROSSEPG ALIAS MOD      *"
+echo "        *                                            *"
+echo "        *  The required files will now be installed  *"
+echo "        *                                            *"
+echo "        **********************************************"
+echo ""
+sleep 5
+
+VIRGIN='/usr/crossepg/scripts/virgin'
+PROVIDER='/usr/crossepg/providers'
+LONDON='https://github.com/jenseneverest/regionals/raw/master/london.zip'
+choice=0
+
+while [ $choice -ne 20 ]
+do
+
+clear
+
+echo "                ###############################################"
+echo "                #  Choose an ITV region from the list below   #"
+echo "                #        Pick a number and press enter        #"
+echo "                #                                             #"
+echo "                #  1) Anglia                                  #"
+echo "                #  2) Tyne & Tees                             #"
+echo "                #  3) Border                                  #"
+echo "                #  4) Westcountry West                        #"
+echo "                #  5) Westcountry SW                          #"
+echo "                #  6) Wales                                   #"
+echo "                #  7) Meridian                                #"
+echo "                #  8) Central West                            #"
+echo "                #  9) London                                  #"
+echo "                # 10) Granada                                 #"
+echo "                #                                             #"
+echo "                #                                             #"
+echo "                # 19) My Area is not listed - help            #"
+echo "                #                                             #"
+echo "                # 20) QUIT                                    #"
+echo "                ###############################################"
+
+echo ""
+echo -n "                Choice: "
+read choice
+
+case "$choice" in
+ 1 ) ./virgin_alias_update.sh ;;
+ 2 ) ./folder/another ;;
+ 3 ) ../progs/onemore ;;
+ 4 ) wget $LONDON && unzip london.zip && sleep 5 && cd london && cp -f update.py /$VIRGIN && cp -f update_script.conf /$PROVIDER && echo "completed" && sleep 4 ;;
+ 20 ) clear; exit 1 ;;
+esac
+
+done
+
