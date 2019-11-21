3a86f4a69410f72802b49c7a5d9e37167efc85bb
jenseneverest <jenseneverest@yahoo.com>
2019-11-21 22:55:43 +0000

    Add files via upload

--- choice.sh
+++ choice.sh
@@ -4,43 +4,51 @@ echo ""
 echo "        **********************************************"
 echo "        *                                            *"
 echo "        *     WELCOME TO THE CROSSEPG ALIAS MOD      *"
-echo "        *                                            *"
-echo "        *  The required files will now be installed  *"
+echo "        *           THIS ADDS VIRGIN EPG             *"
+echo "        *       Proceding to installation now......  *"
 echo "        *                                            *"
 echo "        **********************************************"
 echo ""
 sleep 5
+clear
+echo "        **********************************************"
+echo "        *                                            *"
+echo "        *     Checking for crossepg plugin......     *"
+echo "        *                                            *"
+echo "        **********************************************"
+sleep 3
 
 VIRGIN='/usr/crossepg/scripts/virgin'
 PROVIDER='/usr/crossepg/providers'
 LONDON='https://github.com/jenseneverest/regionals/raw/master/london.zip'
 choice=0
 
-while [ $choice -ne 20 ]
+while [ $choice -ne 22 ]
 do
 
 clear
 
-echo "                ###############################################"
-echo "                #  Choose an ITV region from the list below   #"
-echo "                #        Pick a number and press enter        #"
-echo "                #                                             #"
-echo "                #  1) Anglia                                  #"
-echo "                #  2) Tyne & Tees                             #"
-echo "                #  3) Border                                  #"
-echo "                #  4) Westcountry West                        #"
-echo "                #  5) Westcountry SW                          #"
-echo "                #  6) Wales                                   #"
-echo "                #  7) Meridian                                #"
-echo "                #  8) Central West                            #"
-echo "                #  9) London                                  #"
-echo "                # 10) Granada                                 #"
-echo "                #                                             #"
-echo "                #                                             #"
-echo "                # 19) My Area is not listed - help            #"
-echo "                #                                             #"
-echo "                # 20) QUIT                                    #"
-echo "                ###############################################"
+echo "                ####################################################"
+echo "                #   Choose an ITV region from the list below       #"
+echo "                #         Pick a number and press enter            #"
+echo "                #                                                  #"
+echo "                #  1) Anglia                                       #"
+echo "                #  2) Tyne & Tees                                  #"
+echo "                #  3) Border                                       #"
+echo "                #  4) Westcountry West                             #"
+echo "                #  5) Westcountry SW                               #"
+echo "                #  6) Wales                                        #"
+echo "                #  7) Meridian                                     #"
+echo "                #  8) Central West                                 #"
+echo "                #  9) London                                       #"
+echo "                # 10) Granada                                      #"
+echo "                #                                                  #"
+echo "                #                                                  #"
+echo "                # 19) My Area is not listed - help                 #"
+echo "                #                                                  #"
+echo "                # 20) QUIT                                         #"
+echo "                # 22) REBOOT BOX                                   #"
+echo "                ####################################################"
 
 echo ""
 echo -n "                Choice: "
@@ -51,11 +59,10 @@ case "$choice" in
  2 ) ./folder/another ;;
  3 ) ../progs/onemore ;;
  9 ) wget $LONDON && unzip london.zip && sleep 5 && cd london && cp -f update.py /$VIRGIN && cp -f update_script.conf /$PROVIDER && 
-echo "                ***********************************************"
-echo "                *                                             *"
-echo "                *       YOUR ITV REGION HAS BEEN SETUP        *"
-echo "                *                                             *"
-echo "                ***********************************************"
+echo "           ************************************************************"
+echo "           *          YOUR ITV REGION HAS BEEN SET TO: LONDON         *"
+echo "           *  FUTURE UPDATES TO THE ALIAS FILE VIA PLUGIN NOW SORTED  *"
+echo "           ************************************************************"
 sleep 4 ;;
  20 ) clear; exit 1 ;;
 esac
