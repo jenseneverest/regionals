c4d20bef11fe3e763b48a458bd704220b6d3511d
jenseneverest <jenseneverest@yahoo.com>
2019-11-28 12:10:34 +0000

    Add files via upload

---
+++ clean.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+###########################################################
+get_mounts() {
+    mount | awk '$5 ~ "vfat|^ext" {print $3}'
+}
+sleep 3
+init 4
+sleep 3
+rm -f /etc/enigma2/epg.dat
+for mp in `get_mounts`; do
+    rm -f ${mp}/epg.dat  
+    rm -rf ${mp}/crossepg
+done  
+
+
+
+
+
